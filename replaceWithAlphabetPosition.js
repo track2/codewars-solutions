4c7ecef412270b88103c7826d2a621f84fa0bc45
track2 <track2@gmail.com>
2020-02-28 23:59:33 +0200

    best practices solution

--- replaceWithAlphabetPosition.js
+++ replaceWithAlphabetPosition.js
@@ -1,12 +1,7 @@
 function alphabetPosition(text) {
-  let res = "";
-  const isLetter = n =>
-    n.toLowerCase().charCodeAt(0) >= 97 && n.toLowerCase().charCodeAt(0) <= 122;
-  for (i = 0; i < text.length; i++) {
-    if (isLetter(text[i])) {
-      const pos = text[i].toLowerCase().charCodeAt(0) - 96;
-      res += pos + " ";
-    }
-  }
-  return res.trim();
+  return text
+    .toUpperCase()
+    .match(/[A-Z]/gi)
+    .map(char => char.charCodeAt(0) - 64)
+    .join(" ");
 }
