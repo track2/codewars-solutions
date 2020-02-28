771e33e72a14c560302c2b365a8ff018db6aaded
track2 <track2@gmail.com>
2020-02-29 00:08:28 +0200

    best solution

--- replaceWithAlphabetPosition.js
+++ replaceWithAlphabetPosition.js
@@ -1,7 +1,5 @@
 function alphabetPosition(text) {
-  return text
-    .toUpperCase()
-    .match(/[a-z]/gi)
+  return (text.toUpperCase().match(/[a-z]/gi) || [])
     .map(char => char.charCodeAt() - 64)
     .join(" ");
 }
