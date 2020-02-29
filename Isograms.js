2263ca863d2d0cd1fc60282a70cbdde4213d908b
track2 <track2@gmail.com>
2020-02-29 14:22:55 +0200

    best practice solution

--- Isograms.js
+++ Isograms.js
@@ -1,11 +1,3 @@
 function isIsogram(str) {
-	if (str.split('').length === 0) return true;
-	return (
-		str
-			.toLowerCase()
-			.split('')
-			.filter(
-				c => str.toLowerCase().indexOf(c) !== str.toLowerCase().lastIndexOf(c)
-			).length === 0
-	);
+	return !/(\w).*\1/i.test(str);
 }
